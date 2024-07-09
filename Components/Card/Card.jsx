25036eed88bac2d669bb9e53a92f0148571e21e7
Michael <micaheledquilan@gmail.com>
2024-07-08 21:10:53 -0400

    inital card components

---
+++ Components/Card/Card.jsx
@@ -0,0 +1,22 @@
+
+/*
+Card 2 people - Michael/ Al
+Pokemon Name
+Pokemon Type
+Pokemon image
+Pokemon info
+abilities
+
+*/
+import Title from "./Title"
+import Image from "./Image"
+import Info from "./Info"
+export default function Card() {
+    return (
+        <>
+            <Title />
+            <Image />
+            <Info />
+        </>
+    )
+}
