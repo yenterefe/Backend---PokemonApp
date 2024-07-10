f41841c18f98a72d84112c1ca87d7f9463e2f369
Michael <micaheledquilan@gmail.com>
2024-07-10 19:11:08 -0400

    update

--- Components/Card/Card.jsx
+++ Components/Card/Card.jsx
@@ -15,22 +15,20 @@ export default function Card() {
     return (
         <>
             <div className="card card-compact bg-base-100 w-96 shadow-xl">
+                <Title />
                 <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                        alt="Shoes" />
+                    <Image />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <Info />
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
             </div>
-            <Title />
-            <Image />
-            <Info />
+
+
+
         </>
     )
 }
