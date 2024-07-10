208d83091699a250262ed50894731d53e5b697a1
Michael <micaheledquilan@gmail.com>
2024-07-10 19:05:48 -0400

    install daisy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Card from '../Components/Card/Card'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <Card />
     </>
   )
