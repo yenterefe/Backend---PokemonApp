6b443b0e64bd94211d5ba13f0dadfd50781fc984
tomthedon <thomasasefa19@yahoo.com>
2024-07-11 19:41:01 -0400

    t

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import Card from "./components/Card/Card";
 import Title from "./Title";
 import { SearchBar } from "./components/Searchbar/Searchbar";
-
+import Button from "./Button";
 function App() {
   return (
     <>
       <Title />
       <SearchBar />
+      <Button/>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Card />
     </>
