501c8d1f3f0493265f69a3d92906a8563c48688e
myharton <168491250+myharton@users.noreply.github.com>
2024-07-11 19:07:38 -0400

    added search bar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import "./SearchBar.css";
+
+export const SearchBar = () => {
+    return (
+        <div className="input-wrapper">
+            <input type = "text"  placeholder="Find your Pokemon!" />
+        </div>
+    );
+    
+}
