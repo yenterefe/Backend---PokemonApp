a1fa2d40176f3ac49ca90ecf62230d1eed1f3fdc
yenterefe <yenterefe@gmail.com>
2024-07-11 20:24:51 -0400

    API

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
-// import "./SearchBar.css";
+import { useState } from 'react';
+import axios from 'axios';
+
+
 
 export const SearchBar = () => {
+    const [input, setInput] = useState();
+
+    async function getPokemon() {
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`);
+            alert(JSON.stringify(response.data));
+
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
     return (
-        <div className="input-wrapper">
-            <input type = "text"  placeholder="Find your Pokemon!" />
-        </div>
+        <>
+            <div className="input-wrapper">
+                <input type="text" value={input} placeholder="Find your Pokemon!" onChange={(e) => setInput(e.target.value)} />
+            </div>
+            <button onClick={getPokemon} class="btn btn-primary">
+                Pokemon
+            </button>
+        </>
+
     );
-    
+
 }
