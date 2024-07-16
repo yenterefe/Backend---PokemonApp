e4783b563c1f2617d24a17306d853ae8e4da3e6f
yenterefe <yenterefe@gmail.com>
2024-07-16 19:04:11 -0400

    Hero updated

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,8 +4,19 @@ import { Link } from 'react-router-dom';
 export default function Hero() {
     return (
         <>
-            <h1> This is a hero page</h1>
-            <Link to="/Pokesearch"><button> Go</button></Link>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Welcome to The Pokemon App</h1>
+                        <p className="py-6">
+                            We have summoned the power of all of the pokemon for you to access and gain knowledge into the depths of the WORLD of The Pokemon!
+                        </p>
+
+                        <h1> This is a hero page</h1>
+                        <Link to="/Pokesearch"><button className="btn btn-primary">Get Started</button></Link>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
