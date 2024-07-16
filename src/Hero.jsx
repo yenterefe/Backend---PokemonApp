90ec9f90d2efb5b8472630c218c02189f87c97f8
yenterefe <yenterefe@gmail.com>
2024-07-16 19:42:16 -0400

    fixed hero and footer

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,18 +1,20 @@
 import { Link } from 'react-router-dom';
+import img4 from "./assets/img4.jpg";
 
 
 export default function Hero() {
     return (
         <>
             <div className="hero bg-base-200 min-h-screen">
-                <div className="hero-content text-center">
-                    <div className="max-w-md">
-                        <h1 className="text-5xl font-bold">Welcome to The Pokemon App</h1>
+                <div className="hero-content flex-col lg:flex-row">
+                    <img
+                        src={img4}
+                        className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Latest Pokemon News!</h1>
                         <p className="py-6">
-                            We have summoned the power of all of the pokemon for you to access and gain knowledge into the depths of the WORLD of The Pokemon!
+                            We take you on a Tour to learn the depths of each Pokemon and uncover their unique attributes!
                         </p>
-
-                        <h1> This is a hero page</h1>
                         <Link to="/Pokesearch"><button className="btn btn-primary">Get Started</button></Link>
                     </div>
                 </div>
